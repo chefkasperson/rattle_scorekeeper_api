e75a165614d39c5bbfc6f2bb2185098b3612e4b6
github username <github email address>
2020-05-11 17:51:33 -0700

    added player scores to game model

--- db/migrate/20200512004806_create_games.rb
+++ db/migrate/20200512004806_create_games.rb
@@ -4,6 +4,9 @@ class CreateGames < ActiveRecord::Migration[6.0]
       t.integer :player_1
       t.integer :player_2
       t.integer :player_3
+      t.integer :p1_score
+      t.integer :p2_score
+      t.integer :p3_score
       t.integer :winner_id
       t.string :status
       t.integer :dealer
