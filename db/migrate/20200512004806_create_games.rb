acb99e3a7d89a18754131a9e61bfca9d8e419a5b
github username <github email address>
2020-05-11 17:48:44 -0700

    added game model

---
+++ db/migrate/20200512004806_create_games.rb
@@ -0,0 +1,19 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.integer :player_1
+      t.integer :player_2
+      t.integer :player_3
+      t.integer :winner_id
+      t.string :status
+      t.integer :dealer
+      t.integer :games_played
+      t.integer :most_wins
+      t.integer :total_sets
+      t.integer :most_sets
+      t.integer :average_game_length
+
+      t.timestamps
+    end
+  end
+end
