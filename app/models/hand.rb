cd0e768419477a70ca8b3d7d0915fec646587796
github username <github email address>
2020-05-11 23:12:19 -0700

    added validations

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -1,4 +1,10 @@
 class Hand < ApplicationRecord
   belongs_to :game
   has_many :users, through: :game
+
+  validates :bid, presence: true
+  validates :bidder_id, presence: true
+  validates :p1_score, presence: true
+  validates :p2_score, presence: true
+  validates :p3_score, presence: true
 end
