3e0f3501c3430d7e778470666b48cd86b70f8002
github username <github email address>
2020-05-17 04:29:05 -0700

    Create action and player params

--- app/controllers/Api/v1/players_controller.rb
+++ app/controllers/Api/v1/players_controller.rb
@@ -8,8 +8,19 @@ class Api::V1::PlayersController < ApplicationController
   end
 
   def create
-
+    player = Player.new(player_params)
+    if player.save
+      render json: player.as_json(except: [:created_at, :updated_at])
+    else
+      render json: {error: player.errors.full_messages}
+    end
+    
   end
 
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
 
 end
